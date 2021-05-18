e811430891961a4aeaa0f090a699556a5e399caf
JoanRibot <jribot@cifpfbmoll.eu>
2021-05-18 16:59:34 +0200

    tratamiento pedido internacional

--- mordorcommand/src/main/java/edu/pong/mvpigs/commandpattern/tratamientos/TratamientoPedidoInternacional.java
+++ mordorcommand/src/main/java/edu/pong/mvpigs/commandpattern/tratamientos/TratamientoPedidoInternacional.java
@@ -1,5 +1,22 @@
 package edu.pong.mvpigs.commandpattern.tratamientos;
 
-public class TratamientoPedidoInternacional {
+import edu.pong.mvpigs.commandpattern.interfaces.TratamientoPedido;
+import edu.pong.mvpigs.commandpattern.pedidos.PedidoInternacional;
 
+public class TratamientoPedidoInternacional implements TratamientoPedido {
+
+    private final PedidoInternacional pedido;
+
+    public TratamientoPedidoInternacional(PedidoInternacional pedido) {
+        this.pedido = pedido;
+    }
+
+	@Override
+    public boolean tratar() {
+        return ! pedido.destino().equals("Mordor");
+    }
+
+    public PedidoInternacional getPedido() {
+        return this.pedido;
+    }
 }
