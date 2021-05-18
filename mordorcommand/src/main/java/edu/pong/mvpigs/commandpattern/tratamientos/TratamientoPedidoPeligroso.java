cc14c975c3a38d8ae6f344b55cbb51ec8c499060
JoanRibot <jribot@cifpfbmoll.eu>
2021-05-18 16:55:48 +0200

    pedido peligroso

--- mordorcommand/src/main/java/edu/pong/mvpigs/commandpattern/tratamientos/TratamientoPedidoPeligroso.java
+++ mordorcommand/src/main/java/edu/pong/mvpigs/commandpattern/tratamientos/TratamientoPedidoPeligroso.java
@@ -1,5 +1,22 @@
 package edu.pong.mvpigs.commandpattern.tratamientos;
 
-public class TratamientoPedidoPeligroso {
+import edu.pong.mvpigs.commandpattern.interfaces.PedidoPeligroso;
+import edu.pong.mvpigs.commandpattern.interfaces.TratamientoPedido;
+
+public class TratamientoPedidoPeligroso implements TratamientoPedido{
+
+    private final PedidoPeligroso pedido;
+
+	public TratamientoPedidoPeligroso(PedidoPeligroso pedido) {
+		this.pedido = pedido;
+	}
+
+	public boolean tratar() {
+		return ! pedido.instrucciones().equals("No ponerselo en el dedo");
+	}
+
+	public PedidoPeligroso getPedido() {
+		return this.pedido;
+	}
     
 }
